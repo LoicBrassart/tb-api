3882c07d54914a4558e4c3c43c6b1070153fff78
Loic <loic.brassart@gmail.com>
2021-05-16 10:02:57 +0200

    Initial commit (+conf?)

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use('/', (req, res) => {
+  res.status(404).send('Coming soon!');
+});
+
+app.listen(5050, () => {
+  console.log('Terra Battle API now available on http://localhost:5050 !');
+});
