c6b138e33099f8746b5bf0b074acabeb83158a84
Loic <loic.brassart@gmail.com>
2021-05-16 11:59:19 +0200

    Try to add warny code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use('/', (req, res) => {
-  res.status(404).send('Coming soonish!');
+  res.status(404).send('Coming soonish! ');
 });
 
 app.listen(5050, () => {
