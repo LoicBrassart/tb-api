c27cc528631f5a5b729bc52562db099d6b14b102
Loic <loic.brassart@gmail.com>
2021-05-17 09:04:54 +0200

    Functional GET/characters route with search option

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const cors = require('cors');
+const { db } = require('./conf');
 
 const app = express();
+app.use(cors());
+
+app.get('/characters', async (req, res) => {
+  const { needle } = req.query;
+  let query = 'SELECT * FROM characters';
+
+  if (needle) {
+    query += ` WHERE name LIKE ?`;
+  }
+
+  const [rows] = await db.query(query, [`%${needle}%`]);
+  res.send(rows);
+});
 
 app.use('/', (req, res) => {
-  res.status(404).send('Coming soonish! ');
+  res.status(404).send('Route not found! ');
 });
 
 app.listen(5050, () => {
